c50e8ccd76b92879426a16e107a48f9b1d9948cc
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-08-29 20:15:44 -0400

    all tests passing

--- test/models/shoot_test.rb
+++ test/models/shoot_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class ShootTest < ActiveSupport::TestCase
   test "shoot must have a slug" do
-    @shoot = shoots(:missing_title)
+    @shoot = shoots(:missing_slug)
     refute @shoot.save
-    @shoot.slug = 'OK, You Have a Title'
+    @shoot.slug = 'OK, You Have a Slug'
     assert @shoot.save
   end
 end
