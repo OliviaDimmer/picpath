4eb4c99eadddbb6414a1661959bb515fa63ce665
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-09-12 20:49:57 -0500

    removed current user for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/create'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   resources :shoots
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
