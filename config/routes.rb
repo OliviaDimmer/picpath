f2a5c776f0ec11cd28bb95919e3bdbbd96eab659
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-09-12 20:40:29 -0500

    cleaned up forms, created sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
   resources :shoots
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'shoots#index'
 end
