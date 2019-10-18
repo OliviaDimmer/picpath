54a45f0bc96d3123c9f5dff9d34f6440e2acf710
Olivia Dimmer <Olivia.dimmer@gmail.com>
2019-10-17 19:42:37 -0500

    fixed routes for ical export

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.7'
 # Google Auth
 gem 'google-api-client', require: 'google/apis/calendar_v3'
 gem 'omniauth-google-oauth2'
+gem 'icalendar'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#logout', as: 'logout'
   get '/auth/:provider/callback', to: 'sessions#googleAuth', via: [:get, :post]
   get 'auth/failure', to: redirect('/')
-  get '/ics_export', to: 'shoots#ics_export', as: 'ics_export'
-  resources :shoots
-  resources :users
+  get '/shoots/ics_export/:id', to: 'shoots#ics_export', as: 'ics_export'
+  resources :shoots, :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'shoots#index'
 end
