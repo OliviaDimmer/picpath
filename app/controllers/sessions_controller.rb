32c2742ec734e9e289af57ab32e8f09421ce1bcb
Olivia Dimmer <Olivia.dimmer@gmail.com>
2020-01-14 20:48:03 -0600

    rails g mailer UserMailer, creating a welcome email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   protect_from_forgery except: :create
-  def create
+  def googleAuth
     # Get access tokens from the google server
     access_token = request.env["omniauth.auth"]
     user = User.from_omniauth(access_token)
