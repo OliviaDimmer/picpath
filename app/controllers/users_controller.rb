05bdde61dbe2462d4dc37e211e98b25921f39b83
Olivia Dimmer <Olivia.dimmer@gmail.com>
2020-01-14 20:50:31 -0600

    made mailer respond to user create in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,19 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+          if @user.save
+            # Tell the UserMailer to send a welcome email after save
+            UserMailer.with(user: @user).welcome_email.deliver_later
+
+            format.html { redirect_to(@user, notice: 'User was successfully created.') }
+            format.json { render json: @user, status: :created, location: @user }
+          else
+            format.html { render action: 'new' }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
       end
     end
-  end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
