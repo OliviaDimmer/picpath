c64066f4562d60893ca6b3aa7822ced7d4c96165
Olivia Dimmer <Olivia.dimmer@gmail.com>
2020-02-18 18:37:44 -0600

    added mailer view which needs to be tweaked still

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,5 +7,15 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Welcome to PicPath!')
   end
 
+  def daily_stakeholder_events_to_user(user)
+      user.email = "email@example.com"
+      subject = "Your daily roundup"
+      @user = user
+      @shoots = []
+
+      email_with_name = "#{@user.name} <#{@user.email}>"
+      puts "Emailing #{subject} to #{email_with_name}"
+      mail(:to => email_with_name, :subject => subject)
+    end
 
 end
