fb917910e894671f14d3a871ae315df2b00874b5
Olivia Dimmer <Olivia.dimmer@gmail.com>
2020-03-03 18:43:19 -0600

    added route for google_auth_url for later

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # get '/login', to: 'sessions#login', as: 'login'
   get '/login', to: redirect('/auth/google_oauth2')
   get '/logout', to: 'sessions#logout', as: 'logout'
-  get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  get '/auth/google_oauth2', as: 'google_auth'
+  get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get '/shoots/ics_export/:id', to: 'shoots#ics_export', as: 'ics_export'
   resources :shoots, :users
